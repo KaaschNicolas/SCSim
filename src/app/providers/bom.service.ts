32df06d1f1706ff355329c3b6fd418c1efd9a1af
Nicolas <Nicolas.kaasch@live.de>
2023-12-04 23:33:31 +0100

    init items Controller

--- src/app/providers/bom.service.ts
+++ src/app/providers/bom.service.ts
@@ -1,5 +1,29 @@
 import { Injectable } from '@nestjs/common';
+import { EntityManager, Repository } from 'typeorm';
 import { ForecastContainerDto, ForecastDto, ItemContainerDto, ItemDto } from '../dto';
+import { Item } from 'src/entity';
+import { InjectRepository } from '@nestjs/typeorm';
 
 @Injectable()
-export class BomService {}
+export class BomService {
+    constructor(
+        @InjectRepository(Item)
+        private readonly itemRepository: Repository<Item>,
+        private readonly entityManager: EntityManager,
+    ) {}
+
+    public async createItems(itemContainerDto: ItemContainerDto) {
+        itemContainerDto.itemList.forEach(async (it) => {
+            let item = new Item(it);
+            await this.entityManager.save(item);
+        });
+    }
+
+    public async findAll() {
+        return await this.itemRepository.find();
+    }
+
+    public async findOne(itemNumber: number) {
+        return await this.itemRepository.findOneBy({ itemNumber });
+    }
+}
--- src/app/providers/providers.module.ts
+++ src/app/providers/providers.module.ts
@@ -1,7 +1,8 @@
 import { Module } from '@nestjs/common';
 import { CapacityService } from './capacity.service';
+import { BomService } from './bom.service';
 
 @Module({
-    providers: [CapacityService],
+    providers: [CapacityService, BomService],
 })
 export class ProviderModule {}
