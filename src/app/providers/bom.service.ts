d3418a17af4919e5ab8963d03c42f185c1eabe77
Nicolas <Nicolas.kaasch@live.de>
2023-12-06 18:40:21 +0100

    init bomService

--- src/app/providers/bom.service.ts
+++ src/app/providers/bom.service.ts
@@ -12,19 +12,48 @@ export class BomService {
         private readonly entityManager: EntityManager,
     ) {}
 
-    public async createItems(itemContainerDto: ItemContainerDto) {
+    public async upsertItems(itemContainerDto: ItemContainerDto) {
         itemContainerDto.itemList.forEach(async (it) => {
             let item = new Item(it);
             await this.entityManager.save(item);
         });
     }
 
+    public async findOne(itemNumber: number) {
+        return await this.itemRepository.findOneBy({ itemNumber });
+    }
+
     public async findAll() {
-        //BOM logic here
+        this.resolveBom();
         return await this.itemRepository.find();
     }
 
-    public async findOne(itemNumber: number) {
-        return await this.itemRepository.findOneBy({ itemNumber });
+    private async resolveBom() {
+        //BOM logic here
+        let products = await this.itemRepository.find({
+            where: [{ itemNumber: 1 }, { itemNumber: 2 }, { itemNumber: 3 }],
+        });
+
+        //productionOrder for p1, p2, p3 must be prefilled (forecast)
+        products.forEach(async (item) => {
+            await this.resolveChildren(item, item.productionOrder);
+        });
+    }
+
+    private async resolveChildren(item: Item, parentProdctionOrder: number) {
+        //await this.setChildrenProductionOrder(item);
+        item.productionOrder = parentProdctionOrder;
+        item.productionOrder += item.safetyStock - item.warehouseStock - item.waitingQueue - item.workInProgress;
+        await this.entityManager.save(item);
+
+        let childreen = await this.entityManager.getTreeRepository(Item).findDescendants(item);
+
+        childreen.forEach(async (child) => {
+            if (child === null) {
+                return;
+            }
+
+            await this.resolveChildren(child, item.productionOrder);
+        });
     }
 }
