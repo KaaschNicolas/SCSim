060d06d7ef1c0281bbae5ce3a50596fce5d5132f
Nicolas <Nicolas.kaasch@live.de>
2023-12-13 14:27:54 +0100

    merge dev

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,11 +2,12 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { ConfigModule } from '@nestjs/config';
+import { ProviderModule } from './app/providers/providers.module';
 //import { DatabaseModule } from './database/database.module';
 
 //imports: [ConfigModule.forRoot({ isGlobal: true }), DatabaseModule],
 @Module({
     controllers: [AppController],
-    providers: [AppService],
+    providers: [ProviderModule],
 })
 export class AppModule {}
